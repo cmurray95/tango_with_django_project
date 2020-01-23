85938751ac784837119b30bd3e39af11d2095452
Craig Murray <craig.murray095@gmail.com>
2020-01-23 14:28:59 +0000

    Added about page

--- rango/urls.py
+++ rango/urls.py
@@ -3,7 +3,8 @@ from rango import views
 
 app_name = 'rango'
 
+# Put paths for /rango/<url> here
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
-
