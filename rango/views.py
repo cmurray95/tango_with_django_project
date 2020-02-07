0a6c6308c20306d459886f8b1258b3149b643d56
Craig Murray <craig.murray095@gmail.com>
2020-02-07 12:38:18 +0000

    Added slugification of category urls

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
+    # Retrieve top 5 categories from DB
+    category_list = Category.objects.order_by('-likes')[:5]
     # Template vars
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
 
